6081a43231e831968260595d5ff4d2a596e8c718
danielkim713 <danielkim713@gmail.com>
2018-02-10 10:58:13 -0800

    prep work

--- public/server.js
+++ public/server.js
@@ -2,7 +2,3 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
-
-app.get('/', function(req, res, next) {
-    res.send("Hello world");
-});
